66346eb9315d3f7e129333f562a0f6fbeaf0eb12
Alisson <alidias@stj.jus.br>
2020-10-29 23:36:22 -0300

    Configurando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { HomeComponent } from './pages/home/home.component';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { config } from 'rxjs';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbANXmuqLRmu2qySNIA5RR4vII7jJ9fB0',
+  authDomain: 'playningcards.firebaseapp.com',
+  databaseURL: 'https://playningcards.firebaseio.com',
+  projectId: 'playningcards',
+  storageBucket: 'playningcards.appspot.com',
+  messagingSenderId: '183328916491',
+  appId: '1:183328916491:web:d0835f6aee4b57ccdcb30e',
+  measurementId: 'G-WSYLJW8WLD'
+};
 
 @NgModule({
   declarations: [
@@ -18,10 +34,15 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule
   ],
 
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
